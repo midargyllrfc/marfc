695f1a408dcb61e78962804ef86fabfc6c047324
patrickgrey <websemantic@yahoo.co.uk>
2024-10-04 11:25:13 +0100

    Fixed css references

--- _back-end/build.mjs
+++ _back-end/build.mjs
@@ -5,15 +5,17 @@ import glob from "fast-glob";
   let entryPointsJS = await glob(`website-build/**/*.js`);
   let entryPointsCSS = await glob(`website-build/**/*.css`);
   let entryPoints = [...entryPointsJS, ...entryPointsCSS];
+  // let entryPoints = [...entryPointsJS];
 
   await build({
     allowOverwrite: true,
     bundle: true,
+    loader: { '.woff': 'file', '.woff2': 'file' },
     entryPoints,
     external: ["*.jpg", "*.jpeg", "*.png", "*.svg", "*.webp", "*.avif", "*.gif"],
     sourcemap: true,
     minify: true,
-    nodePaths: [`website-build/_shared`],
+    // nodePaths: [`website-build/_shared`],
     outdir: `website-build`
   });
 })();
